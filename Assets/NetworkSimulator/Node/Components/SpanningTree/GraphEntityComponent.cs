19355d5e9d738e3ff818e155325413a04e7f0e7b
Raggames <enzoboudart@gmail.com>
2024-03-07 22:44:31 +0100

    w

--- Assets/NetworkSimulator/Node/Components/SpanningTree/GraphEntityComponent.cs
+++ Assets/NetworkSimulator/Node/Components/SpanningTree/GraphEntityComponent.cs
@@ -110,6 +110,15 @@ namespace Atom.Components.GraphNetwork
                     _fragmentData.FragmentMembers.AddRange(WorldSimulationManager.nodeAddresses[packet.senderAdress].graphEntityComponent._fragmentData.FragmentMembers);
                 }
 
+                // NEW FRAGMENT MASTER SET ?
+                for(int i = 0; i < _fragmentData.FragmentMembers.Count; ++i)
+                {
+                    var entity = WorldSimulationManager.nodeAddresses[_fragmentData.FragmentMembers[i].peerAdress];
+                   entity.graphEntityComponent._fragmentData.FragmentOwnerId = _fragmentData.FragmentOwnerId;
+                   entity.graphEntityComponent._fragmentData.FragmentLevel = _fragmentData.FragmentLevel;
+                   entity.graphEntityComponent._fragmentData.FragmentMembers = _fragmentData.FragmentMembers;
+                }
+
                 var response = (FragmentJoiningRequestValidated)packet.GetResponsePacket(packet);
                 response.FragmentOwnerAdress = _networkHandling.LocalPeerInfo.peerAdress;
                 response.FragmentOwnerId = _networkHandling.LocalPeerInfo.peerID;
@@ -120,7 +129,7 @@ namespace Atom.Components.GraphNetwork
             }
             else
             {               
-                if (packet.senderID == _fragmentData.MinimumOutgoingEdgePeer.Item2.peerID)
+                if (packet.senderID == _fragmentData.MinimumOutgoingEdgePeer.Item2?.peerID)
                 {
                     Debug.LogError(_networkHandling.LocalPeerInfo.peerAdress + "merging" + packet.senderAdress);
 
@@ -137,6 +146,13 @@ namespace Atom.Components.GraphNetwork
                     _fragmentData.FragmentMembers.AddRange(mergedMembers);
                     _fragmentData.FragmentLevel++;
 
+                    for (int i = 0; i < _fragmentData.FragmentMembers.Count; ++i)
+                    {
+                        var entity = WorldSimulationManager.nodeAddresses[_fragmentData.FragmentMembers[i].peerAdress];
+                        entity.graphEntityComponent._fragmentData.FragmentOwnerId = _fragmentData.FragmentOwnerId;
+                        entity.graphEntityComponent._fragmentData.FragmentLevel = _fragmentData.FragmentLevel;
+                        entity.graphEntityComponent._fragmentData.FragmentMembers = _fragmentData.FragmentMembers;
+                    }
                     // create a new fragment level + 1 with all members from the two merging fragments
                     // respond MERGING
 
@@ -160,7 +176,7 @@ namespace Atom.Components.GraphNetwork
             // simplified version for now, without messaging but direct calculus
             var outerConnections = new List<(PeerInfo, PeerInfo)>();
 
-            /*for (int i = 0; i < _fragmentData.FragmentMembers.Count; ++i)
+            for (int i = 0; i < _fragmentData.FragmentMembers.Count; ++i)
             {
                 var crtmember = WorldSimulationManager.nodeAddresses[_fragmentData.FragmentMembers[i].peerAdress];
                 for (int j = 0; j < crtmember.networkHandling.Connections.Count; ++j)
@@ -168,14 +184,21 @@ namespace Atom.Components.GraphNetwork
                     var connpinfo = crtmember.networkHandling.Connections.ElementAt(j);
                     var conmember = WorldSimulationManager.nodeAddresses[connpinfo.Value.peerAdress];
                     if (conmember.graphEntityComponent._fragmentData.FragmentOwnerId != _fragmentData.FragmentOwnerId)
-                        outerConnections.Add(connpinfo.Value);
+                        outerConnections.Add(new(crtmember.networkHandling.LocalPeerInfo, connpinfo.Value));
                 }
-            }*/
+            }
 
-            for (int i = 0; i < WorldSimulationManager.nodeAddresses.Count; ++i)
+/*            for (int i = 0; i < WorldSimulationManager.nodeAddresses.Count; ++i)
             {
                 var crtmember = WorldSimulationManager.nodeAddresses.ElementAt(i);
-                if(crtmember.Value.graphEntityComponent._fragmentData.FragmentOwnerId == _fragmentData.FragmentOwnerId)
+
+                if (crtmember.Value.graphEntityComponent._fragmentData == null)
+                    Debug.LogError(1);
+
+                if (_fragmentData == null)
+                    Debug.LogError(2);
+
+                if (crtmember.Value.graphEntityComponent._fragmentData.FragmentOwnerId == _fragmentData.FragmentOwnerId)
                 {
                     // local moe
                     var crtOuterConnections = new List<(PeerInfo, PeerInfo)>();
@@ -197,7 +220,7 @@ namespace Atom.Components.GraphNetwork
                     }
                 }
             }
-
+*/
             if (outerConnections.Count > 0)
             {
                 var best = float.MinValue;
@@ -227,6 +250,7 @@ namespace Atom.Components.GraphNetwork
             }
             else
             {
+                _fragmentData.MinimumOutgoingEdgePeer = (null, null);
                 Debug.Log("No outer connection found from fragment " + _fragmentData.FragmentOwnerId);
             }
         }
